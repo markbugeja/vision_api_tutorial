7d3813508b1d733c19b8313a3255f98c6a9c47a2
markbugeja <mk.bugeja@gmail.com>
2019-05-09 10:06:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,12 @@ def GreyScale():
     img = cv2.imread("naturo-monkey-selfie.jpg",1);
     img_str = cv2.imencode('.jpg', img)[1].tostring()
     # build a response dict to send back to client
-    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])}
+    # build a response dict to send back to client
+    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
+                }
     # encode response using jsonpickle
     response_pickled = jsonpickle.encode(response)
+
     return Response(response=response_pickled, status=200, mimetype="application/json")
 	
     
