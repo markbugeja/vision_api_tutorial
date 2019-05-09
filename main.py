e0fd66d84bcb167707d312bbc3442f32f9a94bac
markbugeja <mk.bugeja@gmail.com>
2019-05-09 09:09:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+from flask import Flask, request, Response
+import jsonpickle
+import numpy as np
+import cv2
 
+# Initialize the Flask application
+app = Flask(__name__)
+
+
+@app.route('/api/GreyScale', methods=['POST'])
+def GreyScale():
+    r = request
+    # convert string of image data to uint8
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+	greyimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # do some fancy processing here....
+
+    # build a response dict to send back to client
+    response = {'message': 'image received. size={}x{}'.format(greyimg.shape[1], greyimg.shape[0])
+                }
+    # encode response using jsonpickle
+    response_pickled = jsonpickle.encode(response)
+
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+
+# start flask app
+app.run(host='127.0.0.1', port=8080, debug=True)
